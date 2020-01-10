fb0a8a37601ee20e6c04106f954c2b89be70e7e1
xarkes <antide.petit@gmail.com>
2020-01-10 15:52:48 +0100

    Added support for files that are in $HOME
    Added radare2 and git configurations

--- 0run.sh
+++ 0run.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 
 CONF_DIR=~/.config
-BACKUP="nvim awesome termite alacritty picom"
+BACKUP="nvim awesome termite alacritty picom git"
+BACKUPROOT=".radare2rc .gitconfig"
+
+install_files() {
+  # Backup files that are in .config
+  for dir in ${BACKUP}; do
+    if [ -e "${CONF_DIR}/${dir}" ]; then
+      mv "${CONF_DIR}/${dir}" "${CONF_DIR}/${dir}.old"
+    fi
+    cp -r "${dir}" "${CONFIG_DIR}/${dir}"
+  done
+  # Backup files that are in ~/.*
+  for f in ${BACKUPROOT}; do
+    if [ -e "${HOME}/${f}" ]; then
+      mv "${HOME}/${f}" "${HOME}/${f}.old"
+    fi
+    cp "${f}" "${HOME}/${f}"
+  done
+}
+
+backup_files() {
+  # Save files that are in .config
+  for dir in ${BACKUP}; do
+    cp -r "${CONF_DIR}/${dir}" ./
+  done
+  # Save files that are in ~/.*
+  for f in ${BACKUPROOT}; do
+    cp -r "${HOME}/$f" ./
+  done
+
+  # Don't backup wallpapers
+  rm awesome/xarkes/*.{png,jpg}
+}
 
 if [[ "$1" = "install" ]]; then
-  for dir in ${BACKUP}; do
-    if [ -d ${CONF_DIR}/${dir} ]; then
-      mv ${CONF_DIR}/${dir} ${CONF_DIR}/${dir}.old
-    fi
-    cp -r ${dir} ${CONFIG_DIR}/${dir}
-  done
+  install_files
 else
-  for dir in ${BACKUP}; do
-    cp -r ${CONF_DIR}/${dir} ./
-  done
+  backup_files
 fi
 
